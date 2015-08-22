f5d7a95c9015da1d9302f51225e92a1ca6b120e2
zerosquadron <skip@zerosquadron.com>
2015-08-22 12:53:03 -0700

    cleaned up

--- src/level1.js
+++ src/level1.js
@@ -2,10 +2,23 @@
 'use strict';
 
 const Level = {
-	waypoints 		: [{x:120, y:0}, {x:120, y:138}, {x:904, y:138}, {x:904, y:229}, {x:120, y:229}, {x:120, y:354}, {x:904, y:354}, {x:904, y:470}, {x:120, y:470}, {x:120, y:523}]
+	waypoints 		: [
+					{x:120, y:0}
+					, {x:120, y:138}
+					, {x:904, y:138}
+					, {x:904, y:229}
+					, {x:120, y:229}
+					, {x:120, y:354}
+					, {x:904, y:354}
+					, {x:904, y:470}
+					, {x:120, y:470}
+					, {x:120, y:523}
+					]
+
 	, background 	: 'background'
 	, mobs 			: [{spriteKey: 'king'}
 					, {spriteKey: 'knight'}]
+					
 	, props 		: [
 			{x:100,y:100, spriteKey:'tree'}
 			, {x:0,y:0, spriteKey: 'tree'}
