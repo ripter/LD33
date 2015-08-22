87d30d403a3ca61cf20b209c2af6d9a54de0b0c6
Cid Donham <cdubs8600@gmail.com>
2015-08-22 13:20:53 -0700

    no message

--- dist/main.js
+++ dist/main.js
@@ -344,7 +344,7 @@
 					background: 'background',
 					mobs: [{ spriteKey: 'king' }, { spriteKey: 'knight' }],
 
-					props: [{ x: 100, y: 100, spriteKey: 'tree' }, { x: 0, y: 0, spriteKey: 'tree' }, { x: 0, y: 0, spriteKey: 'tree' }, { x: 0, y: 50, spriteKey: 'wall' }]
+					props: [{ x: 116, y: 160, spriteKey: 'wall' }, { x: 180, y: 160, spriteKey: 'wall' }, { x: 244, y: 160, spriteKey: 'wall' }, { x: 308, y: 160, spriteKey: 'wall' }, { x: 372, y: 160, spriteKey: 'wall' }, { x: 638, y: 160, spriteKey: 'tree' }, { x: 744, y: 160, spriteKey: 'tree' }, { x: 868, y: 160, spriteKey: 'tree' }, { x: 498, y: 250, spriteKey: 'tree' }, { x: 435, y: 378, spriteKey: 'tree' }, { x: 638, y: 378, spriteKey: 'tree' }, { x: 745, y: 378, spriteKey: 'shrub' }, { x: 806, y: 490, spriteKey: 'shrub' }, { x: 645, y: 490, spriteKey: 'tree' }, { x: 237, y: 490, spriteKey: 'shrub' }]
 	};
 
 	exports['default'] = Level;
